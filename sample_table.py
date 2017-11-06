f8ac57dee44ac9a60bdf98bb88d086af2f3073b2
carleton <cjc@lanl.gov>
2017-11-05 20:02:30 -0700

    adding helper script for making sample tables

---
+++ sample_table.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import sys, argparse, json
+
+# prints a line to standard error
+def print_err(data):
+    sys.stderr.write(str(data)+'\n')
+
+def main(args):
+    print_err('loading: {}'.format(args.sample_data))
+    with open(args.sample_data) as file:
+        data = json.load(file)
+
+    for solution_data in data['solutions']:
+        row = [solution_data['num_occurrences']] + solution_data['solution']
+        print(', '.join([str(x) for x in row]))
+
+def build_cli_parser():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('sample_data', help='a data file to operate on (.json)')
+
+    return parser
+
+
+if __name__ == '__main__':
+    parser = build_cli_parser()
+    main(parser.parse_args())
