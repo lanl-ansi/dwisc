7c57b6c87def5eb2b3e323b09b860a352e8bea35
ccoffrin <carleton@coffrin.com>
2020-06-17 12:31:50 -0600

    update anneal-schedule parameter name

--- dwisc.py
+++ dwisc.py
@@ -209,7 +209,7 @@ def build_cli_parser():
     parser.add_argument('-as', '--auto-scale', help='have d-wave rescale the problem', action='store_true', default=False)
     parser.add_argument('-srtr', '--spin-reversal-transform-rate', help='the number of reads to take before each spin reversal transform', type=int)
     parser.add_argument('-fdc', '--flux-drift-compensation', help='enable flux drift compensation', action='store_true', default=False)
-    parser.add_argument('-ans', '--anneal-schedule', help='an array of annealing schedule pairs', nargs='+', type=schedule_pair)
+    parser.add_argument('-asch', '--anneal-schedule', help='an array of annealing schedule pairs', nargs='+', type=schedule_pair)
 
     return parser
 
